e112604231346cdb078ffcb1d2372cc4e4281fb9
Carlos Narvaez <carlos.narvaez@mambu.com>
2024-10-25 18:55:23 -0500

    Terraform updates

--- UalaAccounting/UalaAccounting.infra/terraform/main.tf
+++ UalaAccounting/UalaAccounting.infra/terraform/main.tf
@@ -509,8 +509,8 @@ resource "aws_api_gateway_resource" "status_orchestrate_ah_resource" {
 
 resource "aws_api_gateway_method" "status_orchestrate_ah_method" {
   rest_api_id   = aws_api_gateway_rest_api.ah_apigw.id
-  resource_id   = aws_api_gateway_resource.orchestrate_ah_resource.id
-  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.status_orchestrate_ah_resource.id
+  http_method   = "POST"
   authorization = "NONE"
 }
 
@@ -518,7 +518,7 @@ resource "aws_api_gateway_integration" "status_orchestrate_ah_integration" {
   rest_api_id             = aws_api_gateway_rest_api.ah_apigw.id
   resource_id             = aws_api_gateway_resource.status_orchestrate_ah_resource.id
   http_method             = aws_api_gateway_method.status_orchestrate_ah_method.http_method
-  integration_http_method = "GET"
+  integration_http_method = "POST"
   type                    = "HTTP_PROXY"
   uri                     = "http://${aws_lb.load_balancer.dns_name}/api/Execute/statusorchestrate"
 
